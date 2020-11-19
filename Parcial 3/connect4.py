9e8b3a4d70290ddd018b8fa043fbe71830e9f40c
ChebaCheba <bastiangaxiola@gmail.com>
2020-11-18 22:07:35 -0600

    Complete game with Negamax

--- Parcial 3/connect4.py
+++ Parcial 3/connect4.py
@@ -1,4 +1,21 @@
+#------------------------------------------------------------------------------------------------------------------
+#   Tic Tac Toe game.
+#
+#   This code is an adaptation of the Tic Tac Toe bot described in:
+#   Artificial intelligence with Python. Alberto Artasanchez and Prateek Joshi. 2nd edition, 2020, 
+#   editorial Pack. Chapter 13.
+#
+#------------------------------------------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------------------------------------------
+#   Imports
+#------------------------------------------------------------------------------------------------------------------
+
+from easyAI import TwoPlayersGame, Human_Player, AI_Player, Negamax
 from itertools import groupby, chain
+#------------------------------------------------------------------------------------------------------------------
+#   Class definitions
+#------------------------------------------------------------------------------------------------------------------
 
 NONE = '.'
 RED = 'R'
@@ -14,40 +31,54 @@ def diagonalsNeg (matrix, cols, rows):
     for di in ([(j, i - cols + j + 1) for j in range(cols)] for i in range(cols + rows - 1)):
         yield [matrix[i][j] for i, j in di if i >= 0 and j >= 0 and i < cols and j < rows]
 
-class Game:
-    def __init__ (self, cols = 7, rows = 6, requiredToWin = 4):
-        """Create a new game."""
+class ConnectFour(TwoPlayersGame):
+    """ Class that is used to play the TIC TAC TOE game. """
+
+    def __init__(self, players, cols = 7, rows = 6, requiredToWin = 4):
+        """ 
+            This constructor initializes the game according to the specified players.
+
+            players : The list with the player objects.
+        """
         self.cols = cols
         self.rows = rows
         self.win = requiredToWin
-        self.board = [[NONE] * rows for _ in range(cols)]
+        # Define the players
+        self.players = players
 
-    def insert (self, column, color):
-        """Insert the color in the given column."""
-        c = self.board[column]
+        # Define who starts the game
+        self.nplayer = 1 
+
+        # Define the board
+        self.board = [[NONE] * rows for _ in range(cols)]
+    
+    def show(self):
+        """Print the board."""
+        print('  '.join(map(str, range(self.cols))))
+        for y in range(self.rows):
+            print('  '.join(str(self.board[x][y]) for x in range(self.cols)))
+        print()
+
+    def possible_moves(self):
+        """ This method returns the possible moves according to the current game state. """        
+        return [x for x in range(self.cols) if self.board[x][0]==NONE]
+    
+    def make_move(self, move):
+        """ 
+            This method executes the specified move.
+
+            move : The move to execute.
+        """
+        #self.board[int(move) - 1] = self.nplayer
+        c = self.board[move]
 
         if c[0] != NONE:
             # raise Exception('Column is full')
             print("*** Column is full ***")
-            return color
-
         i = -1
         while c[i] != NONE:
             i -= 1
-        c[i] = color
-
-        self.checkForWin()
-        
-        turn = YELLOW if color == RED else RED
-        return turn
-
-    def checkForWin (self):
-        """Check the current board for a winner."""
-        w = self.getWinner()
-        if w:
-            self.printBoard()
-            print('*** ' + w + ' won! ***')
-            exit()
+        c[i] = self.player.name
 
     def getWinner (self):
         """Get the winner on the current board."""
@@ -63,18 +94,36 @@ class Game:
                 if color != NONE and len(list(group)) >= self.win:
                     return color
 
-    def printBoard (self):
-        """Print the board."""
-        print('  '.join(map(str, range(self.cols))))
-        for y in range(self.rows):
-            print('  '.join(str(self.board[x][y]) for x in range(self.cols)))
-        print()
+    def loss_condition(self):
+        """ This method returns whether the opponent has three in a line. """
+        w = self.getWinner()
+        if w:
+            return True
+        else:
+            return False 
+    
+    def is_over(self):
+        """ This method returns whether the game is over. """
+        return (self.possible_moves() == []) or self.loss_condition()
+        
+    def scoring(self):
+        """ This method computes the game score (-100 for loss condition, 0 otherwise). """
+        return -100 if self.loss_condition() else 0
 
+#------------------------------------------------------------------------------------------------------------------
+#   Main function
+#------------------------------------------------------------------------------------------------------------------
+def main():
+
+    # Search algorithm of the AI player
+    algorithm = Negamax(7)
+
+    # Start the game
+    ConnectFour([Human_Player('R'), AI_Player(algorithm, 'Y')]).play()
 
 if __name__ == '__main__':
-    g = Game()
-    turn = RED
-    while True:
-        g.printBoard()
-        row = input('{}\'s turn: '.format('Red' if turn == RED else 'Yellow'))
-        turn = g.insert(int(row), turn)
+    main()
+
+#------------------------------------------------------------------------------------------------------------------
+#   End of file
+#------------------------------------------------------------------------------------------------------------------
